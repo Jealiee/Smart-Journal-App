a6b9396e7e5b29885fa42f361d3ab4ddc1de8592
Jealie <adamus_julia@wp.pl>
2026-03-02 19:16:19 +0100

    Starting on the layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,34 @@
 import { useState } from 'react'
 import { Box, TextField, Button } from '@mui/material'
-import './App.css'
 
 function App() {
   const [inputText, setInputText] = useState('')
 
   return (
-    <Box sx={{ p: 2 }}>
-      <TextField
-        label="Write your journal entry!"
-        variant='outlined'
-        fullWidth
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <Box sx={{ mt: 2 }}>
-        <Button
-          variant="contained"
-          onClick={() => console.log('work in progress...')}>
-          Submit
-        </Button>
+    <Box sx={{ display: 'flex', height: '100vh' }}>
+
+      {/*Sidebar placeholder*/}
+      <Box sx={{ width: 120, bgcolor: 'grey' }}>
+        Sidebar
+      </Box>
+
+      <Box sx={{ ml: 2, width: '40%', maxWidth:600, display: 'flex', flexDirection: 'column', height: '100%', p:1 }}>
+        <TextField
+          label="Write your journal entry!"
+          variant='outlined'
+          multiline
+          minRows={20}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <Box sx={{mt: 2, alignSelf:'flex-end'}}>
+          <Button
+            variant="contained"
+            onClick={() => console.log('work in progress...')}
+          >
+            Submit
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
